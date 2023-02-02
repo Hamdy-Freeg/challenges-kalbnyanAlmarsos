03ee354f90e05fb8a34e73949f5e8bfc027dba11
Hamdy-Freeg <93085428+Hamdy-Freeg@users.noreply.github.com>
2023-02-02 15:26:42 +0200

    Add files via upload

---
+++ JSassignment/dom-assignment-start/assignment.js
@@ -0,0 +1,18 @@
+// Assainment 1
+
+// let task1 = document.querySelector("#task-1");
+let task1 = document.getElementById("task-1");
+
+// Assainment 2
+
+// let title = document.getElementsByTagName("title")[0];
+let title = document.querySelector("title");
+
+title.textContent = "Assignment - Solved!";
+
+// Assainment 3
+
+let h1 = document.getElementsByTagName("h1")[0];
+
+h1.textContent = "Assignment - Solved!";
+h1.style.color = "black";
