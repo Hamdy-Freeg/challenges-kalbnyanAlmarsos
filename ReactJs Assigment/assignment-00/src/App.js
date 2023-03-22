edf77ce982a7a5f0f714d0b88c281b3bc68058de
Hamdy-Freeg <93085428+Hamdy-Freeg@users.noreply.github.com>
2023-03-22 10:38:18 +0200

    Add files via upload

---
+++ ReactJs Assigment/assignment-00/src/App.js
@@ -0,0 +1,38 @@
+import ExpenseComponent from './components/ExpenseComponent';
+
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 3, 1),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2023, 3, 21),
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2023, 3, 25),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2023, 3, 11),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's Get Started</h2>
+      <ExpenseComponent expenses={expenses}></ExpenseComponent>
+    </div>
+  );
+}
+
+export default App;
